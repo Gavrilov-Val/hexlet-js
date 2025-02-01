6756bee6eb109876b3388ea623951ca065efb6f5
Gavrilov-Val <gavrilov.valery@mail.ru>
2025-02-01 08:43:29 -0700

    use eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import _ from "lodash";
+import _ from 'lodash';
+
 const hello = () => {
-    console.log("Hello, World!");
+  console.log('Hello, World!');
 };
 
 const last = () => {
-    console.log(_.last(["one", "two"]));
+  console.log(_.last(['one', 'two']));
 };
 
 export { hello, last };
